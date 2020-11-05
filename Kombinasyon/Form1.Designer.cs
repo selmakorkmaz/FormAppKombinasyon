b6047c402089d46f65e633411022a495238c2c84
Selma Korkmaz <39559295+selmakorkmaz@users.noreply.github.com>
2020-11-05 17:09:19 +0300

    added

---
+++ Kombinasyon/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Kombinasyon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 76);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(506, 409);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button1.Location = new System.Drawing.Point(12, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Excel Dosyası Aç";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(521, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Tüm Kombinasyonlar";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(13, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Tekil Grup Kodları";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.dataGridView2.Location = new System.Drawing.Point(524, 76);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(512, 409);
+            this.dataGridView2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1048, 497);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
+
---
+++ Kombinasyon/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Kombinasyon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OpenFileDialog file;
+        public DataTable getirExcelTablo()
+        {
+            OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + file.FileName + ";Extended Properties=Excel 12.0;");
+            baglanti.Open();
+            string query = "select * from [Sayfa1$]";
+            OleDbDataAdapter oAdp = new OleDbDataAdapter(query, baglanti);
+            DataTable dt = new DataTable();
+            oAdp.Fill(dt);
+            dataGridView1.DataSource = dt;
+            return dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            file = new OpenFileDialog();
+            file.Filter = "Excel Dosyası |*.xlsx";
+            file.ShowDialog();
+
+            DataTable dt = getirExcelTablo();
+
+            List<List<object>> okunanList = new List<List<object>>();
+            List<object> objectArr1 = new List<object>();
+            List<object> objectArr2 = new List<object>();
+            List<object> objectArr3 = new List<object>();
+            List<object> objectArr4 = new List<object>();
+            List<object> objectArr5 = new List<object>();
+
+            for (int a = 0; a < dt.Rows.Count; a++)
+            {
+
+                if (dt.Rows[0]["T_GK_1"].ToString() != "")
+                {
+                    if (dt.Rows[a]["T_GK_1"].ToString() != "")
+                    {
+
+                        objectArr1.Add(dt.Rows[a]["T_GK_1"].ToString());
+                    }
+                }
+                if (dt.Rows[0]["T_GK_2"].ToString() != "")
+                {
+
+                    if (dt.Rows[a]["T_GK_2"].ToString() != "")
+                    {
+                        objectArr2.Add(dt.Rows[a]["T_GK_2"].ToString());
+                    }
+                }
+                if (dt.Rows[0]["T_GK_3"].ToString() != "")
+                {
+                    if (dt.Rows[a]["T_GK_3"].ToString() != "")
+                    {
+                        objectArr3.Add(dt.Rows[a]["T_GK_3"].ToString());
+                    }
+                }
+                if (dt.Rows[0]["T_GK_4"].ToString() != "")
+                {
+                    if (dt.Rows[a]["T_GK_4"].ToString() != "")
+                    {
+                        objectArr4.Add(dt.Rows[a]["T_GK_4"].ToString());
+                    }
+                }
+
+                if (dt.Rows[0]["T_GK_5"].ToString() != "")
+                {
+                    if (dt.Rows[a]["T_GK_5"].ToString() != "")
+                    {
+                        objectArr5.Add(dt.Rows[a]["T_GK_5"].ToString());
+                    }
+                }
+            }
+
+            if (objectArr1.Count != 0) okunanList.Add(objectArr1);
+            if (objectArr2.Count != 0) okunanList.Add(objectArr2);
+            if (objectArr3.Count != 0) okunanList.Add(objectArr3);
+            if (objectArr4.Count != 0) okunanList.Add(objectArr4);
+            if (objectArr5.Count != 0) okunanList.Add(objectArr5);
+
+            List<object[]> kombList = new List<object[]>(); // Kombinasyon verilerinin tutulduğu liste
+
+            kombList = Kombinasyon(okunanList, kombList, 0);
+            DataTable dt2 = new DataTable();
+            dt2 = ConvertListToDataTable(kombList);
+            dataGridView2.DataSource = dt2;
+        }
+
+        private static List<object[]> Kombinasyon(List<List<object>> list, List<object[]> newList, params int[] loopInd)
+        {
+            if (loopInd.Length <= list.Count) // loopInd -> Döngü Sayısı. Verilen listenin boyutunu geçmemeli.
+            {
+                int currentCount = list[loopInd.Length - 1].Count;
+
+                while (loopInd[loopInd.Length - 1] < currentCount) // i1<list[0] , i2<list[1] , i3<list[2]
+                {
+                    Kombinasyon(list, newList, loopInd.Concat(new[] { 0 }).ToArray()); // iç döngü için 
+                    loopInd[loopInd.Length - 1]++; // i++, i2++ , i3++ ...
+                }
+            }
+            else
+            {
+                int j = 0;
+                object[] temp = loopInd.Take(loopInd.Length - 1).Select(i => list[j++][i]).ToArray();
+                newList.Add(temp);
+            }
+            return newList;
+        }
+
+        public DataTable ConvertListToDataTable(List<object[]> list)
+        {
+            DataTable table = new DataTable();
+            int columns = 0;
+            foreach (var array in list)
+            {
+                if (array.Length > columns)
+                {
+                    columns = array.Length;
+                }
+            }
+            // Column Ekle.
+            for (int i = 0; i < columns; i++)
+            {
+                table.Columns.Add();
+            }
+            // Row Ekle.
+            foreach (var array in list)
+            {
+                table.Rows.Add(array);
+            }
+            return table;
+        }
+    }
+}
